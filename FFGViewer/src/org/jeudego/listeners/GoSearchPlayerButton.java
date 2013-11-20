98a6c8e8f34929a743d8d7985b95a23fe4ee7444
SimeonG <simeon.girousse@gmail.com>
2013-11-20 19:12:51 +0100

    Added constructor search player button

--- FFGViewer/src/org/jeudego/listeners/GoSearchPlayerButton.java
+++ FFGViewer/src/org/jeudego/listeners/GoSearchPlayerButton.java
@@ -16,6 +16,13 @@ public class GoSearchPlayerButton implements OnClickListener{
 	private List<Player> _player_list;
 	private PlayerQuickViewAdapter _player_quick_view_adapter;
 	
+	public GoSearchPlayerButton(EditText name_edit_text, EditText surname_edit_text, List<Player> player_list, PlayerQuickViewAdapter player_quick_view_adapter){
+		this._name_edit_text = name_edit_text;
+		this._surname_edit_text = surname_edit_text;
+		this._player_list = player_list;
+		this._player_quick_view_adapter = player_quick_view_adapter;
+	}
+	
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
